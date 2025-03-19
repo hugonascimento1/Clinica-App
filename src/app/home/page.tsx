13a2234a615b4927306fac9ece8a9e08b852f961
Hugo Nascimento <hugovinitek@gmail.com>
2025-03-19 13:36:39 -0300

    .

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,7 +12,7 @@ function HomePage() {
 
       <div className="flex flex-col justify-center items-center gap-6 my-10 sm:w-full p-4">
         <Link
-          href="/conteudo1"
+          href=""
           className="flex flex-row w-full sm:w-3/4 md:w-1/2 h-20 sm:h-24 md:h-28 bg-[#9F3EB8] justify-center items-center gap-3 sm:gap-5 text-white text-lg sm:text-xl md:text-2xl font-medium rounded-lg shadow-md"
         >
           <FilePenLineIcon className="w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8" />
@@ -20,7 +20,7 @@ function HomePage() {
         </Link>
 
         <Link
-          href=""
+          href="home/chat"
           className="flex flex-row w-full sm:w-3/4 md:w-1/2 h-20 sm:h-24 md:h-28 bg-[#9F3EB8] justify-center items-center gap-3 sm:gap-5 text-white text-lg sm:text-xl md:text-2xl font-medium rounded-lg shadow-md"
         >
           <BotIcon className="w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8" />
